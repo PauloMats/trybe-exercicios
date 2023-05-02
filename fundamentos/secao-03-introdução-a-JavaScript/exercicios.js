12420ed0371e5ecb5e48f25da449a57e6cbe2842
Paulo Mateus <pm.mats98@gmail.com>
2023-05-02 18:17:07 -0300

    declaracao de variaveis e operadores

---
+++ fundamentos/secao-03-introdução-a-JavaScript/exercicios.js
@@ -0,0 +1,15 @@
+let a = 10
+let b = 2
+let adi;
+let sub;
+let mult;
+let div;
+let mod;
+
+adi = (a + b);
+sub = (a - b);
+mult = (a * b);
+div = (a / b); 
+mod = (a % b);
+
+console.log(adi, sub, mult, div, mod);
