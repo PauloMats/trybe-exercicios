a345492dbfc765c292bb354d47da0963b954bc57
Paulo Mateus <pm.mats98@gmail.com>
2023-05-02 16:00:47 -0300

    correção da sintaxe do case

--- fundamentos/secao-03-introdução-a-JavaScript/exercicios-de-fixação.js
+++ fundamentos/secao-03-introdução-a-JavaScript/exercicios-de-fixação.js
@@ -6,15 +6,15 @@ switch (resultado) {
         console.log("Parabéns, você está no grupo de pessoas aprovadas!");
         break;
 
-    case ("lista");
+    case ("lista"):
         console.log("Você está na nossa lista de espera.”");
         break;
 
-    case ("reprovada");
+    case ("reprovada"):
     console.log ("nfelizmente, você reprovou.");
         break;
 
-    default; 
+    default:
         console.log ("Informação incorreta");
         
 }
