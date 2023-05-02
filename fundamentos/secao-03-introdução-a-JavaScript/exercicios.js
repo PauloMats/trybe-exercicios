20a12d56d27d2bff4a41d4ec1b811d0c2bd2ecd1
Paulo Mateus <pm.mats98@gmail.com>
2023-05-02 18:24:13 -0300

    resolução requisito 3

--- fundamentos/secao-03-introdução-a-JavaScript/exercicios.js
+++ fundamentos/secao-03-introdução-a-JavaScript/exercicios.js
@@ -1,5 +1,6 @@
-let a = 10
-let b = 2
+let a = 3;
+let b = 2;
+let c = 5;
 let adi;
 let sub;
 let mult;
@@ -14,8 +15,10 @@ mod = (a % b);
 
 console.log(adi, sub, mult, div, mod);
 
-if (a > b){
+if (a > b && a > c){
     console.log("O maior número é: " + a);
-} else {
+} else if (b > a && b > c){
     console.log("O maior número é: " + b);
+} else {
+    console.log("O maior número é: " + c);
 }
