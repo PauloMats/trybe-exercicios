f452e8baffe44b21833aa4b8c13be231f61dfb6d
Paulo Mateus <pm.mats98@gmail.com>
2023-05-03 12:51:39 -0300

    exercicio de fixação 1 colcuido, e 2 iniciado

---
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-02-JS-array-e-loop-for/exercicio_fixaçao1.js
@@ -0,0 +1,6 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+let indexOfPortfolio = menu.length['Portfólio'];
+
+console.log(indexOfPortfolio);
+console.log(menuServices);
