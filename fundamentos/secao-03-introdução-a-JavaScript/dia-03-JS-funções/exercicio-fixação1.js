10eeb7c8c179742f63006a6ea71840803a557a13
Paulo Mateus <pm.mats98@gmail.com>
2023-05-05 08:56:32 -0300

    criação de arquivo para testar os gabaritos

--- fundamentos/secao-03-introdução-a-JavaScript/dia-03-JS-funções/exercicio-fixação1.js
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-03-JS-funções/exercicio-fixação1.js
@@ -1,6 +1,14 @@
-
+let cliente = "João";
+let saldo = 5000.00;
 
 function trybeBank(cliente) {
 
-    return "Olá,", cliente , ". Seu saldo é:", saldo,"R$."
-}
+    return "Olá,", cliente ,".";
+
+};
+function valorBank(saldo){
+    return "Seu saldo é:", saldo,"R$.";
+};
+
+console.log(trybeBank('João'));
+console.log(valorBank("5000,00"));
