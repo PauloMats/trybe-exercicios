d14b89e64c42c84188db51b6060324e2c9043595
Paulo Mateus <pm.mats98@gmail.com>
2023-05-07 15:11:02 -0300

    resolução do requisito primeiro. uhuuu

---
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-04-JS-Objetos/script.js
@@ -0,0 +1,13 @@
+//Crie uma variável player e atribua a ela um objeto que contenha as variáveis listadas na sequência.
+
+let player = {
+    name: 'Mats',
+    lastName: 'Silva',
+    age: 25,
+    medals: {
+        golden: 3,
+        silver: 5,
+    }
+}
+
+console.log('O Jogador ' + player.name + ' tem ' + player.age + ' anos de idade.');
