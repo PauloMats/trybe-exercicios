d217726313344853c78556176169c0c9f3b138a7
Paulo Mateus <pm.mats98@gmail.com>
2023-05-07 16:05:57 -0300

    resolução do ultimo exercicio de fixacao do for in e farofa

--- fundamentos/secao-03-introdução-a-JavaScript/dia-04-JS-Objetos/script2.js
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-04-JS-Objetos/script2.js
@@ -9,3 +9,13 @@ let names = {
   for (let i in names){
     console.log('Olá ' + names[i]);
   }
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let i in car){
+    console.log(i, car[i]);
+  }
