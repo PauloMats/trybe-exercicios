4efcd60314117983d0a36f946d9081bcb36b420f
Paulo Mateus <pm.mats98@gmail.com>
2023-05-07 15:32:31 -0300

    resolução do ultimo requisito e termino do fixacao

--- fundamentos/secao-03-introdução-a-JavaScript/dia-04-JS-Objetos/script.js
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-04-JS-Objetos/script.js
@@ -12,7 +12,8 @@ let player = {
 
 console.log('O Jogador ' + player.name + ' tem ' + player.age + ' anos de idade.');
 
-player.bestInTheWorld = ['[2006, 2007, 2008, 2009, 2010, 2018]'];
+player.bestInTheWorld = ['2006, 2007, 2008, 2009, 2010, 2018'];
 
-console.log('O Jogador ' + player.name + ' foi eleito o melhor do mundo por 6 vezes!' + player['bestInTheWorld']);
+console.log('O Jogador ' + player.name + ' foi eleito o melhor do mundo por 6 vezes! ' + player['bestInTheWorld']);
 
+console.log('O jogador ' + player.name +  ' Possui ' + player.medals.golden + ' de ouro e ' + player.medals.silver + '  medalhas de prata.');
