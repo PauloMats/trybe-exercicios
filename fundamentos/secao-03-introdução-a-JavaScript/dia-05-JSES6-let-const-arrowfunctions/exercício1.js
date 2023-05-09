52287a5fe1b4f29cc366801a3fc011fb323c741d
Paulo Mateus <pm.mats98@gmail.com>
2023-05-09 09:21:05 -0300

    Criação de diretório do dia 5 de JS ES6 e finalização do primeiro exercício :rocket:

---
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-05-JSES6-let-const-arrowfunctions/exercício1.js
@@ -0,0 +1,11 @@
+//🚀 Modifique a variável para que respeite o escopo em que está sendo declarada.
+
+function imprimeIdade() {
+    for (let idade = 30; idade <= 40; idade += 1) {
+      console.log('Idade dentro do for:', idade)
+    }
+
+  }
+  imprimeIdade();
+
+
