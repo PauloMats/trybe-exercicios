fcfbeb02845128d7019c159ec5121b7141e7a8c4
Paulo Mateus <pm.mats98@gmail.com>
2023-05-09 09:51:11 -0300

    resolução dos demais exercicios e inicio dos exercicios de arrow functions

--- fundamentos/secao-03-introdução-a-JavaScript/dia-05-JSES6-let-const-arrowfunctions/exercício1.js
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-05-JSES6-let-const-arrowfunctions/exercício1.js
@@ -8,6 +8,8 @@ function imprimeIdade() {
   }
   imprimeIdade();
 
+
+
 // 🚀 Altere o valor das propriedades do objeto para que respeite as características da variável do tipo const.
 
   // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
@@ -21,3 +23,29 @@ function imprimeIdade() {
   } // Altere essa estrutura para corrigir o erro.
   console.log('Nome:', pessoa.nome);
   console.log('Idade:', pessoa.idade);
+
+
+  //🚀 Modifique a variável para que não ocorra Erro.
+
+  let favoriteFood = 'Lasanha';
+  favoriteFood = 'Hambúrguer';
+  console.log(favoriteFood);
+
+  //🚀 Modifique as concatenações para template literals
+
+  const name = 'Adriana';
+  const lastName = 'Soares';
+  console.log(` Olá ${name} ${lastName} !`);
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de a + b é: ${a+b}`);
+
+  //🚀 Modifique a estrutura das funções a seguir para que elas sejam arrow functions.
+
+  const numeroAleatorio = numeroAleatorio => Math.random();
+    
+  console.log(numeroAleatorio());
