b22442ba9cfcd48466ae2aec2d6a6aecabde82ec
Paulo Mateus <pm.mats98@gmail.com>
2023-05-09 09:33:52 -0300

    resolução requisito 2 e inicio do 3

--- fundamentos/secao-03-introdução-a-JavaScript/dia-05-JSES6-let-const-arrowfunctions/exercício1.js
+++ fundamentos/secao-03-introdução-a-JavaScript/dia-05-JSES6-let-const-arrowfunctions/exercício1.js
@@ -8,4 +8,16 @@ function imprimeIdade() {
   }
   imprimeIdade();
 
+// 🚀 Altere o valor das propriedades do objeto para que respeite as características da variável do tipo const.
 
+  // Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+  const pessoa = {
+    nome: 'Henri',
+    idade: 20
+  }
+  pessoa2 = {
+    nome: 'Luna',
+    idade: 19
+  } // Altere essa estrutura para corrigir o erro.
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
