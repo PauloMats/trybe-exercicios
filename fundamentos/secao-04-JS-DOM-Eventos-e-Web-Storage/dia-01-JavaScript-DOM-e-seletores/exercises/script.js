710e5b9375f57ab737ab91c439ae84074a664043
Paulo Mateus <pm.mats98@gmail.com>
2023-05-13 15:39:27 -0300

    inicio da resolução do exercicio da tabela de prioridades trybe

---
+++ fundamentos/secao-04-JS-DOM-Eventos-e-Web-Storage/dia-01-JavaScript-DOM-e-seletores/exercises/script.js
@@ -0,0 +1,11 @@
+const nomesTabela1 = document.querySelector('.emergency-tasks');
+
+nomesTabela1.style.color = 'White';
+
+const fundoTabela1 = document.querySelectorAll('div')[0, 1];
+
+nomesTabela1.style.background = "#FF9F84";
+
+const fundoNomes = document.querySelectorAll('h3')[0, 1, 2, 3];
+
+fundoNomes.style.backgroundColor = 'red'
