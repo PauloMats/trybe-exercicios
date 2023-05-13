3e428c351e07af67ce253d59ac0e688e5425883d
Paulo Mateus <pm.mats98@gmail.com>
2023-05-13 15:06:44 -0300

    resolução dos exercicios de fixação

---
+++ fundamentos/secao-04-JS-DOM-Eventos-e-Web-Storage/dia-01-JavaScript-DOM-e-seletores/script.js
@@ -0,0 +1,5 @@
+let pilotosF1Atual = document.getElementsByClassName('piloto-f1-atual');
+
+for (let i = 0; i < pilotosF1Atual.length; i += 1){
+    console.log(pilotosF1Atual[i].innerText)
+}
---
+++ fundamentos/secao-04-JS-DOM-Eventos-e-Web-Storage/dia-01-JavaScript-DOM-e-seletores/script2.js
@@ -0,0 +1,5 @@
+const paragrafos = document.getElementsByClassName('paragraph')[0];
+paragrafos.style.color = "red";
+
+const subtitle = document.getElementsByTagName('h4')[0];
+subtitle.style.color = "red";
