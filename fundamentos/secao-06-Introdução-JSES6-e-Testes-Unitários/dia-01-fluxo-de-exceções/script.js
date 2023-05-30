76bf9b65f536664e076569225e177922f9e29986
Paulo Mateus <pm.mats98@gmail.com>
2023-05-30 15:29:33 -0300

    cria repositorio de exercicios da aula do módulo 6 dia 2

--- fundamentos/secao-06-Introdução-JSES6-e-Testes-Unitários/dia-01-fluxo-de-exceções/script.js
+++ fundamentos/secao-06-Introdução-JSES6-e-Testes-Unitários/dia-01-fluxo-de-exceções/script.js
@@ -11,3 +11,46 @@ const promo = [
     {product: 'capa protetora de prancha de surf', price: 89.59},
     {product: 'bola de basquete', price: 39.99},
   ];
+
+  const button = document.querySelector('#send-button');
+
+button.addEventListener('click', () => {
+  const name = document.querySelector('#name-id').value;
+  const number = document.querySelector('#number-id').value;
+
+  showPromo(name, number);
+});
+
+const showPromo = (name, number) => {
+  const firstText = document.querySelector('#text-initial');
+  const secondText = document.querySelector('#text-final');
+  
+  try {
+    checkName(name);
+    checkNumber(parseInt(number));
+    checkValidRange(number);
+    const productObject = checkPromo(number);
+
+    firstText.innerHTML = `Boas-vindas, ${name}!`;
+    secondText.innerHTML = `A promoção do dia é: 
+      ${productObject.product} no valor de R$ ${productObject.price}`;
+  } catch(err) {
+    secondText.innerHTML = err.message;
+  } finally {
+  document.querySelector('#name-id').value = "";
+  document.querySelector('#number-id').value = "";
+  }
+}
+const checkName = (name) => {
+  let letters = /[aA-zZ]+/;
+  
+  if (!name.match(letters)) {
+    throw new Error('É necessário digitar um nome válido');
+  }
+}
+
+const checkNumber = (number) => {
+  if (isNaN(number)) {
+    throw new Error('É necessário digitar um número');
+  }
+}
