26488c4feeba400036af974f4d2ac6a127e0f054
PauloMats <pm.mats98@gmail.com>
2023-06-23 11:13:55 -0300

    adiciona arquivos para o gerador de senha

---
+++ front-end/secao-01-Introdução-ao-Frontend-e-JS-assícrono/dia-01-Ambiente-de-desenvolvimento/exercicios/scr/main.js
@@ -0,0 +1,13 @@
+import { nanoid } from 'nanoid'
+
+const passwordBtnEl = document.querySelector("button")
+const displayPasswordEl = document.querySelector("h2")
+
+passwordBtnEl.addEventListener("click", () => {
+    const randomPassword = nanoid()
+    displayPasswordEl.innerHTML = randomPassword
+})
+
+const randomPassword = nanoid()
+
+console.log(randomPassword)
