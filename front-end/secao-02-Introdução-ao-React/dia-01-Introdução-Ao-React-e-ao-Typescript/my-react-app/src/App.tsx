38da3c8e6943f128878b553628301687df851d3e
PauloMats <pm.mats98@gmail.com>
2023-07-06 15:50:46 -0300

    resolve os 5 primeiros requisitos do exercicio, e cria o componente Title e implementa ele ao código

---
+++ front-end/secao-02-Introdução-ao-React/dia-01-Introdução-Ao-React-e-ao-Typescript/my-react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Title from './components/Title/Title'
+
+function App() {
+
+  return (
+    <div>
+      Work in progress
+      <Title />
+      </div>
+  )
+}
+
+export default App
